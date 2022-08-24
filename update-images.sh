ff94a3a835241a7192027564ba1cdcdd3077f375
Kevin Gurton <kevin.gurton@gmail.com>
2022-08-24 11:20:06 +0100

    Remove old images

--- update-images.sh
+++ update-images.sh
@@ -1,3 +1,5 @@
 docker build -t tevity/dev:latest -f dev.Dockerfile .
 docker build -t tevity/angular-dev:latest -f ./angular/Dockerfile .
 docker build -t tevity/nest-dev:latest -f ./nest/Dockerfile .
+docker rmi $(docker images --filter=reference="vsc*" -q) -f
+docker rmi $(docker images -f "dangling=true" -q)
